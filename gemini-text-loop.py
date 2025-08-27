93bba10ac6b4a62673094d81d3535d0cd2673593
schuyler12 <schuyler2367@gmail.com>
2025-08-27 12:23:39 +0530

    Add files via upload
    normal chatting or asking anything from gemini (only prompt)

---
+++ gemini-text-loop.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+
+while True:
+    query = input("Enter your query: ").lower()
+    if query != "none":
+        genai.configure(api_key="GOOGLE GEMINI API KEY HERE")
+        model = genai.GenerativeModel("gemini-2.0-flash-lite") #please check the available models of gemini
+        response = model.generate_content(f"Sir : {query}")
+
+        if response.text:
+            response_text = str(response.text).strip()
+            print(f"NAVIDS: {response_text}")
+        else:
+            raise Exception("Empty response from Gemini")
+            
